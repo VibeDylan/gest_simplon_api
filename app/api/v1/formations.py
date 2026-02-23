c000e7b795d3e212d9654cae942904ab40292ade
VibeDylan <dylanbourbotte62@icloud.com>
2026-02-23 13:52:07 +0100

    create route for formation

---
+++ app/api/v1/formations.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.repositories.formation_repo import FormationRepository
+from app.schemas.formation import FormationCreate, FormationRead, FormationUpdate
+from sqlmodel import Session
+from typing import List
+
+router = APIRouter(prefix="/formations", tags=["formations"])
+
+def get_formation_service(session: Session = Depends(get_session)) -> FormationService:
+    """Injecte session → repository → service pour les routes users."""
+    repo = FormationRepository(session)
+    return FormationService(repo)
